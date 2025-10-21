b33402dce6acfc74170dcb55dbc8424bdecd6dea
VesperQuartz <lilbrown3000@gmail.com>
2025-10-21 17:03:04 +0100

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,35 @@
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { devtools } from "@tanstack/devtools-vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
-const config = defineConfig(({mode}) => {
-	console.log("mode", mode)
+const config = defineConfig(() => {
 	return {
 		server: {
 			allowedHosts: ["*.local"],
 		},
 		plugins: [
+			devtools(),
 			// this is the plugin that enables path aliases
 			viteTsConfigPaths({
 				projects: ["./tsconfig.json"],
 			}),
 			tailwindcss(),
 			tanstackStart(),
-			viteReact(),
+			nitroV2Plugin({
+				preset: "node_server",
+				compatibilityDate: "2025-10-21",
+			}),
+			viteReact({
+				babel: {
+					plugins: [["babel-plugin-react-compiler"]],
+				},
+			}),
 		],
-	}
+	};
 });
 
 export default config;
