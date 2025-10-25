c3523d46cf6fb66c4e0e319b62ac68d7aae68662
VesperQuartz <lilbrown3000@gmail.com>
2025-10-25 21:47:29 +0100

    small fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
+// import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-// import { nitro } from "nitro/vite";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
@@ -20,10 +20,11 @@ const config = defineConfig(() => {
 			}),
 			tailwindcss(),
 			tanstackStart(),
-			nitroV2Plugin({
-				preset: "node_server",
-				compatibilityDate: "2025-10-21",
-			}),
+			nitro(),
+			// nitroV2Plugin({
+			// 	preset: "node_server",
+			// 	compatibilityDate: "2025-10-21",
+			// }),
 			viteReact({
 				babel: {
 					plugins: [["babel-plugin-react-compiler"]],
