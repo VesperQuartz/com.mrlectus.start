8678d51a167e6a47d463a9a7acfc31fc3a274a95
VesperQuartz <lilbrown3000@gmail.com>
2025-12-01 15:45:42 +0100

    updates (fix)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,7 @@ import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 import { serverEnv } from "@/env/server";
 import * as schema from "@/repo/schema";
+import * as authSchema from "@/repo/schema/auth.schema";
 
 // For Node.js - make sure to install the 'ws' and 'bufferutil' packages
 //neonConfig.webSocketConstructor = ws;
@@ -11,6 +12,10 @@ const sql = new Pool({
 	connectionString: String(serverEnv.DB_URL),
 });
 
-export const db = drizzle({ client: sql, logger: true, schema });
+export const db = drizzle({
+	client: sql,
+	logger: true,
+	schema: { ...schema, ...authSchema },
+});
 
 export type Db = typeof db;
