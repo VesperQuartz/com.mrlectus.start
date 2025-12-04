1dd662dac0dfeffdd123af4998ae52e0e6abfa02
VesperQuartz <lilbrown3000@gmail.com>
2025-12-05 00:41:58 +0100

    updates (fix)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
+// import { nitro } from "nitro-nightly/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
