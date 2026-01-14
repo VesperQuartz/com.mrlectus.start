a6e7a92b912fe17b90eb270e8a34de5b84c23215
VesperQuartz <lilbrown3000@gmail.com>
2026-01-14 02:22:03 +0100

    updates (fix)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,32 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { toast } from "sonner";
+import { ClientOnly, createFileRoute } from "@tanstack/react-router";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const App = () => {
 	return (
-		<div className="mt-1 flex flex-col items-center justify-center gap-1">
-			Hello, world
-			<Button onClick={() => toast.success("Hello")}>Hello</Button>
+		<div className="p-4 text-center">
+			<ClientOnly>
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						const formData = new FormData(e.currentTarget);
+						const data = Object.fromEntries(formData.entries());
+						console.log(data);
+					}}
+				>
+					<div className="flex flex-col gap-4">
+						<Input
+							type="text"
+							name="name"
+							placeholder="Name"
+							className="w-fit border border-blue-500"
+						/>
+						<Button type="submit" className={"w-fit cursor-pointer"}>
+							Submit
+						</Button>
+					</div>
+				</form>
+			</ClientOnly>
 		</div>
 	);
 };
